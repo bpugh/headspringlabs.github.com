764101db4f04c41abb59cc1267ca7e25a015c2c7
Chris Missal <chris.missal@gmail.com>
2014-12-10 23:19:18 -0600

    change destination to static instead of dist for easier image access

--- gulpfile.js
+++ gulpfile.js
@@ -13,19 +13,19 @@ gulp.task('vendor-css', function() {
   gulp.src('./static/css/*.css')
     .pipe(concat('vendor.css'))
     .pipe(minifyCss({keepSpecialComments:0}))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./static/'))
 });
 
 gulp.task('vendor-js', function() {  
   return gulp.src(['./static/script/vendor/jquery*.js','./static/script/vendor/*.js'])
     .pipe(concat('vendor.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./static/'))
 });
 
 gulp.task('app-js', function() {  
   return gulp.src('./static/script/app/*.js')
     .pipe(concat('app.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./static/'))
 });
