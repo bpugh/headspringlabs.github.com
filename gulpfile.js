567d1bf4411eba03a54a83300e945935b1048db3
Chris Missal <chris.missal@gmail.com>
2014-12-10 22:07:10 -0600

    introduce gulpfile for front-end tasks

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp')
+,	concat = require('gulp-concat')
+,	minifyCss = require('gulp-minify-css')
+,	uglify = require('gulp-uglify')
+;
+
+
+gulp.task('default', ['vendor-css','vendor-js','app-js'], function() {
+  // place code for your default task here
+});
+
+gulp.task('vendor-css', function() {
+  gulp.src('./static/css/*.css')
+    .pipe(concat('vendor.css'))
+    .pipe(minifyCss({keepSpecialComments:0}))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('vendor-js', function() {  
+  return gulp.src('./static/script/vendor/*.js')
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('app-js', function() {  
+  return gulp.src('./static/script/app/*.js')
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
+});
