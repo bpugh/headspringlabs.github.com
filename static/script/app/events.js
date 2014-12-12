825075ab1000cc13b1d67b2b157e6392969c3bfb
Chris Missal <chris.missal@gmail.com>
2014-12-11 20:12:27 -0600

    update event tracking to use _gaq

--- static/script/app/events.js
+++ static/script/app/events.js
@@ -1,13 +1,13 @@
-(function ($, ga) {
+(function ($, gaq) {
 
 	$(function () {
 		$('.headspringer-modal').on('show.bs.modal', function (e) {
 			var modal = $(e.currentTarget);
-			if (ga && modal && modal.length) {
+			if (modal && modal.length) {
 				var login = modal.data('member-login');
-				login && ga.push(['_trackEvent', 'MemberView', 'Login', login]);
+				gaq.push(['_trackEvent', 'MemberView', 'Login', login]);
 			}
 		})
 	});
 
-})(jQuery, ga);
+})(jQuery, _gaq);
